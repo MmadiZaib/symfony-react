8845da9c76d7016aa58b0ad9e8101164baaacc6d
Mmadi Zaïb <mzaib@manymore.fr>
2022-11-01 22:31:31 +0100

    Allow user with role admin to see all invoices

--- src/Doctrine/CurrentUserExtension.php
+++ src/Doctrine/CurrentUserExtension.php
@@ -8,6 +8,7 @@ use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use App\Entity\Customer;
 use App\Entity\Invoice;
 use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Security\Core\Security;
 
 class CurrentUserExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
@@ -15,16 +16,31 @@ class CurrentUserExtension implements QueryCollectionExtensionInterface, QueryIt
     /** @var Security  */
     private $security;
 
-    public function __construct(Security $security)
+    /** @var AuthorizationCheckerInterface */
+    private $auth;
+
+    public function __construct(Security $security, AuthorizationCheckerInterface $checker)
     {
         $this->security = $security;
+        $this->auth = $checker;
     }
 
     public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
     {
+        $this->addWhere($queryBuilder, $resourceClass);
+    }
+
+    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, string $operationName = null, array $context = [])
+    {
+        $this->addWhere($queryBuilder, $resourceClass);
+    }
+
+
+    private function addWhere(QueryBuilder $queryBuilder, string $resourceClass)
+    {
         $user = $this->security->getUser();
 
-        if ($resourceClass === Customer::class || $resourceClass === Invoice::class) {
+        if (($resourceClass === Customer::class || $resourceClass === Invoice::class) && !$this->auth->isGranted('ROLE_ADMIN')) {
             $rootAlias = $queryBuilder->getRootAliases()[0];
 
             if ($resourceClass === Customer::class) {
@@ -36,9 +52,5 @@ class CurrentUserExtension implements QueryCollectionExtensionInterface, QueryIt
 
             $queryBuilder->setParameter("user", $user);
         }
-    }
-
-    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, string $operationName = null, array $context = [])
-    {
     }
 }
