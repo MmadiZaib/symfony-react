a1023b015c7e8e2bb4f48e8ee6891b264c304566
Mmadi Zaïb <mzaib@manymore.fr>
2022-11-01 22:24:14 +0100

    Create doctrine extension to limit user query

--- src/Doctrine/CurrentUserExtension.php
+++ src/Doctrine/CurrentUserExtension.php
@@ -2,7 +2,43 @@
 
 namespace App\Doctrine;
 
-class CurrentUserExtension
-{
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use App\Entity\Customer;
+use App\Entity\Invoice;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\Security\Core\Security;
 
+class CurrentUserExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
+{
+    /** @var Security  */
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
+    {
+        $user = $this->security->getUser();
+
+        if ($resourceClass === Customer::class || $resourceClass === Invoice::class) {
+            $rootAlias = $queryBuilder->getRootAliases()[0];
+
+            if ($resourceClass === Customer::class) {
+                $queryBuilder->andWhere("$rootAlias.user = :user");
+            } elseif ($resourceClass === Invoice::class) {
+                $queryBuilder->join("$rootAlias.customer", "c")
+                    ->andWhere("c.user = :user");
+            }
+
+            $queryBuilder->setParameter("user", $user);
+        }
+    }
+
+    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, string $operationName = null, array $context = [])
+    {
+    }
 }
