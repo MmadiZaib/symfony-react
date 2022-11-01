c97ac14a507c30ed23a4c1455cc36c9ceeefc95f
Mmadi Zaïb <mzaib@manymore.fr>
2022-11-01 15:26:41 +0100

    Create custom normalizer for DateTimeNormalizer

--- src/Entity/Invoice.php
+++ src/Entity/Invoice.php
@@ -110,7 +110,7 @@ class Invoice
         return $this->sentAt;
     }
 
-    public function setSentAt(DateTimeInterface $sentAt): self
+    public function setSentAt($sentAt): self
     {
         $this->sentAt = $sentAt;
 
