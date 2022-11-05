4e73899937f154527a34b878bb2934f513837198
Mmadi Zaïb <mzaib@manymore.fr>
2022-11-05 21:38:20 +0100

    Design the customer page

--- assets/js/pages/CustomersPage.jsx
+++ assets/js/pages/CustomersPage.jsx
@@ -2,7 +2,43 @@ import React from "react";
 
 const CustomersPage = (props) => {
     return (
-        <h1>Liste des clients</h1>
+        <>
+            <h1>Liste des clients</h1>
+
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Id.</th>
+                        <th>Client</th>
+                        <th>Email</th>
+                        <th>Entreprise</th>
+                        <th className="text-center">Factures</th>
+                        <th className="text-center">Montant total</th>
+                        <th/>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>12</td>
+                        <td>
+                            <a href="#">Mmadi Zaib</a>
+                        </td>
+                        <td>mmadi@mail.io</td>
+                        <td>Mmadi Inc</td>
+                        <td className="text-center">
+                            <span className="badge bg-primary">4</span>
+                        </td>
+                        <td className="text-center">2 400,00 €</td>
+                        <td>
+                            <button className="btn btn-sm btn-danger">Supprimer</button>
+                        </td>
+                    </tr>
+                </tbody>
+
+
+            </table>
+        </>
+
     );
 }
 
