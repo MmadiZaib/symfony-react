152bd6e3a029856af054e35d905bbac09f4a6562
Mmadi Zaïb <mzaib@manymore.fr>
2022-11-28 21:44:14 +0100

    create Field component

---
+++ assets/js/components/forms/Field.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Field = ({name, label, value, onChange, placeHolder = "", type = "text", error = ""}
+) => {
+    return (
+        <div className="form-group">
+            <label htmlFor="{name}">{label}</label>
+            <input
+                type={type}
+                value={value}
+                onChange={onChange}
+                className={"form-control" + (error && " is-invalid")}
+                id={name}
+                name={name}
+                placeholder={placeHolder || label}
+            />
+            {error && <div className="invalid-feedback">
+                {error}
+            </div>}
+        </div>
+    )
+}
+
+export default Field;
